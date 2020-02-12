74a29b62d8f275087268be383a2fa66981f3b4fd
ZhengLiang95 <zhengliangnnc@gmail.com>
2020-02-11 23:24:39 -0700

    Styled

--- project-react-app/src/App.js
+++ project-react-app/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from './component/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header></Header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Dashboard />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
--- project-react-app/src/component/Dashboard.js
+++ project-react-app/src/component/Dashboard.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem'
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateButton from "./Project/CreateButton";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to the Dashboard</h1>
-                <ProjectItem />
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateButton />
+
+              <br />
+              <hr />
+              <ProjectItem />
             </div>
-            
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
